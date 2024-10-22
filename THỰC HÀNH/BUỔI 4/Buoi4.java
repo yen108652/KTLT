861bcd751c007763c893c5813ae551c78cd666fb
yen108652 <yennguyen.tk2402@gmail.com>
2024-10-22 11:40:09 +0700

    Add files via upload

---
+++ THỰC HÀNH/BUỔI 4/Buoi4.java
@@ -0,0 +1,19 @@
+package baitap4;
+
+public class Buoi4 {
+    public static void viDu4() {
+        int arr[] = {3, 5, 8, 10, 14, 25};
+        int cs=0;
+        System.out.printf("Phần tử thứ nhất arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ hai arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ ba arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ tư arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ năm arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ sáu arr[%d] = " + arr[cs], cs++);
+
+    }
+    public static void main(String[] args) {
+        // ToDo Auto-generated method stub
+        viDu4();
+    }
+}
