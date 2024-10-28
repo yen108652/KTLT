6386d07391fbbd8362765925935b3b745f7a04eb
yen108652 <yennguyen.tk2402@gmail.com>
2024-10-28 21:21:15 +0700

    Add files via upload

---
+++ THỰC HÀNH/BUỔI 4/Kyen108652.java
@@ -0,0 +1,14 @@
+package buoi4;
+
+public class Kyen108652 {
+    public static void vidu4() {
+        int arr[] = { 3, 5, 8, 10, 14, 25 };
+        System.out.print("Danh sachs các phần tử của mảng là: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("\t arr[%d] = %d",i, arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        vidu4();
+    }
+}
