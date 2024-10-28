f9d61acb8fafe73e7d6571498768b680bdd9ae86
yen108652 <yennguyen.tk2402@gmail.com>
2024-10-28 21:07:11 +0700

    Add files via upload

--- THỰC HÀNH/BUỔI 4/Yen108652.java
+++ THỰC HÀNH/BUỔI 4/Yen108652.java
@@ -1,14 +1,16 @@
 package buoi4;
 
 public class Yen108652 {
-    public static void bai4_1() {
-        int arr[] = { 3, 5, 8, 10, 14, 25 };
+    public static void vidu5() {
+        String nguHanh[] = { "kim", "moc", "thuy", "hoa", "tho" };
         System.out.print("Danh sách các phần tử của mảng là: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("\t arr[%d] = %d",i, arr[i]);
+        for (int i = 0; i < nguHanh.length; i++) {
+            System.out.printf("\n nguHanh[%d] = %S",i, nguHanh[i]);
+           
         }
+
     }
     public static void main(String[] args) {
-        bai4_1();
+        vidu5();
     }
 }
